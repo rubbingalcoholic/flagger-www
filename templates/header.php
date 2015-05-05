b777afa9e2b36f343b40ec61a7083d62121b920c
Jeff Lyon <jeff@rubbingalcoholic.com>
2015-05-04 22:07:42 -0700

    new piwik instance

--- templates/header.php
+++ templates/header.php
@@ -26,15 +26,14 @@ else
 		<script type="text/javascript">
 			var _paq = _paq || [];
 			<? if (LIVE) { ?>
-				_paq.push(["trackPageView"]);
-				_paq.push(["enableLinkTracking"]);
-
+				_paq.push(['trackPageView']);
+				_paq.push(['enableLinkTracking']);
 				(function() {
-					var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.sharemyshit.com/";
-					_paq.push(["setTrackerUrl", u+"piwik.php"]);
-					_paq.push(["setSiteId", "5"]);
-					var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-					g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+					var u="//piwik.rubbingalcoholic.com/";
+					_paq.push(['setTrackerUrl', u+'piwik.php']);
+					_paq.push(['setSiteId', 1]);
+					var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+					g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 				})();
 			<? } ?>
 		</script>
